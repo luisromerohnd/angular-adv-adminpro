9cbd88fd554681b5e2ea1db52a0358b0035616d1
Luis Romero <luisarnaldo11@gmail.com>
2021-02-15 15:33:13 -0500

    Environment listo

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,15 +1,27 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router, CanLoad, Route, UrlSegment } from '@angular/router';
 import { UsuarioService } from '../services/usuario.service';
 import { tap } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuard implements CanActivate {
+export class AuthGuard implements CanActivate, CanLoad {
 
   constructor(private usuarioService: UsuarioService,
     private router: Router) { }
+
+  canLoad(route: Route, segments: UrlSegment[]): boolean | Observable<boolean> | Promise<boolean> {
+    return this.usuarioService.validarToken()
+    .pipe(
+      tap(estaAutenticado => {
+        if(!estaAutenticado) {
+          this.router.navigateByUrl('/login');
+        }
+      })
+    );
+  }
 
   canActivate(
     next: ActivatedRouteSnapshot,
@@ -22,7 +34,7 @@ export class AuthGuard implements CanActivate {
           this.router.navigateByUrl('/login');
         }
       })
-    )
+    );
   }
 
 }
